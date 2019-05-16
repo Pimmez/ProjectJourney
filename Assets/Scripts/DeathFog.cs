b4451e8d3375d0e02eedf9b5c1e5cd8438c27cb5
DESKTOP-F6IC33L\Pimmez <pimhoreman@gmail.com>
2019-05-16 12:10:28 +0200

    Start Project with: -player movement -Cinemachine camera follow -Deathfog overtime time damage

---
+++ Assets/Scripts/DeathFog.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DeathFog : MonoBehaviour
+{
+	public static Action<float> DeathFogEvent;
+
+	[SerializeField] private float damageRate = 3;
+	[SerializeField] float damageAmount = 1f;
+	private int ticks = 0;
+	private int ticksPerSec = 60;
+
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			ApplyDamageOverTime();
+		}
+	}
+
+	private void ApplyDamageOverTime()
+	{
+		// ticks increments 60 times per second, as an example
+		ticks++;
+		// Condition is true once every second
+		if (ticks % (ticksPerSec * damageRate) == 0)
+		{
+			// Apply damage
+			if (DeathFogEvent != null)
+			{
+				DeathFogEvent(damageAmount);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	private float maxHealth = 100f;
+
+	[SerializeField] private float health;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+		health = maxHealth;
+    }
+
+    // Update is called once per frame
+	private void Update()
+    {
+    
+    }
+
+	private void TakeDamage(float damageAmount)
+	{
+		health -= damageAmount;
+	}
+
+	private void OnEnable()
+	{
+		DeathFog.DeathFogEvent += TakeDamage;
+	}
+
+	private void OnDisable()
+	{
+		DeathFog.DeathFogEvent -= TakeDamage;
+	}
+}
